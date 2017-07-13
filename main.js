2eb619cb4bf10994ace7ed474b0cf4f89b50ad57
Karolina Drazewska <drazewska.karolina@gmail.com>
2017-07-13 12:27:57 +0200

    exercise 1

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
+// new Vue({
+// 	el: '#app',
+// 	data: {
+// 		title: 'Hello hello',
+// 		link: 'http://google.com',
+// 		finishedLink: '<a href="http://google.com">Google link</a>'
+// 	},
+// 	methods: {
+// 		sayHello: function() {
+// 			this.title = 'Hello'
+// 			return this.title;
+// 		}
+// 	}
+// });
+
 new Vue({
-	el: '#app',
+	el: '#exercise',
 	data: {
-		title: 'Hello hello',
-		link: 'http://google.com',
-		finishedLink: '<a href="http://google.com">Google link</a>'
+		name: 'Karolina',
+		age: 28,
+		imgLink: 'http://royalwholesalecandy.com/media/catalog/category/chocolate_3.jpg'
 	},
 	methods: {
-		sayHello: function() {
-			this.title = 'Hello'
-			return this.title;
+		multipleAge: function() {
+			return this.age*3;
+		},
+		random: function() {
+			return Math.random();
 		}
+
 	}
 });
