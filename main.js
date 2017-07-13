081b4536852be2f381ac0e4f6db85b8b7dbe4f80
Karolina Drazewska <drazewska.karolina@gmail.com>
2017-07-13 11:45:43 +0200

    using directives in vue

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 new Vue({
 	el: '#app',
 	data: {
-		title: 'Hello hello'
+		title: 'Hello hello',
+		link: 'http://google.com',
+		finishedLink: '<a href="http://google.com">Google link</a>'
 	},
 	methods: {
-		changeTitle: function(event) {
-			this.title = event.target.value;
+		sayHello: function() {
+			this.title = 'Hello'
+			return this.title;
 		}
 	}
 });
