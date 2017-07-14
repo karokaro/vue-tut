c5a4c3749a0d6804deb5647ad5fd4e443f746149
Karolina Drazewska <drazewska.karolina@gmail.com>
2017-07-14 12:05:20 +0200

    examples of listening to events

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-// new Vue({
-// 	el: '#app',
-// 	data: {
-// 		title: 'Hello hello',
-// 		link: 'http://google.com',
-// 		finishedLink: '<a href="http://google.com">Google link</a>'
-// 	},
-// 	methods: {
-// 		sayHello: function() {
-// 			this.title = 'Hello'
-// 			return this.title;
-// 		}
-// 	}
-// });
+new Vue({
+	el: '#app',
+	data: {
+		title: 'Hello hello',
+		link: 'http://google.com',
+		finishedLink: '<a href="http://google.com">Google link</a>'
+	},
+	methods: {
+		sayHello: function() {
+			this.title = 'Hello'
+			return this.title;
+		}
+	}
+});
 
 new Vue({
 	el: '#exercise',
@@ -28,5 +28,28 @@ new Vue({
 			return Math.random();
 		}
 
+	}
+});
+new Vue({
+	el: '#listeningEvents',
+	data: {
+		counter: 0,
+		x: 0,
+		y: 0
+	},
+	methods: {
+		increase: function(step, event) {
+			this.counter += step;
+		},
+		updateCoordinates: function(event) {
+			this.x = event.clientX;
+			this.y = event.clientY;
+		},
+		alertMe: function() {
+			alert('Alert!');
+		}
+		// dummy: function(event) {
+		// 	event.stopPropagation();
+		// }
 	}
 });
