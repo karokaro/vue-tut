78d369b3321e842a76d703bb62ebdd7855ca1688
Karolina Drazewska <drazewska.karolina@gmail.com>
2017-07-15 07:51:52 +0200

    computed properties & watching for changes

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
 new Vue({
     el: '#app',
     data: {
-        name: 'Marcel'
+        counter: 0,
+        secondCounter: 0
+    },
+    // dependent properties
+    computed : {
+    	output: function() {
+    		return this.counter > 5 ? 'Greater 5' : 'Smaller than 5'
+    	}
+    },
+    // execute code upon data changes
+    watch: {
+    	counter: function(value) {
+    		var vm = this;
+    		setTimeout(function() {
+    			vm.counter = 0;
+    		}, 2000);
+    	}
+    },
+    methods: {
+    	// increase: function() {
+    	// 	this.counter++;
+    	// 	this.result = this.counter > 5 ? 'Greater 5' : 'Smaller 5'
+    	// },
+    	// decrease: function() {
+    	// 	this.counter--;
+    	// 	this.result = this.counter > 5 ? 'Greater 5' : 'Smaller 5'
+    	// }
+    	result: function() {
+    		return this.counter > 5 ? 'Greater 5' : 'Smaller than 5'
+    	}
     }
 });
