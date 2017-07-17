92329ed72a2f5bdc00f4b3ebcdd68b9a0383bc12
Karolina Drazewska <drazewska.karolina@gmail.com>
2017-07-17 10:42:10 +0200

    exercise 3 - reactive properties

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 new Vue({
-    el: '#app',
-    data: {
-        link: 'http://google.com'
-    },
-    methods: {
-    	changeLink: function() {
-    		this.link = 'http://apple.com'
-    	}
-    }
+	el: '#exercise3',
+	data: {
+		value: 0
+	},
+	computed: {
+		result: function() {
+			return this.value >= 37 ? 'done' : 'not there yet';
+		}
+	},
+	watch: {
+		result: function() {
+			var vm = this;
+			setTimeout(function() {
+				vm.value = 0;
+			}, 5000);
+		}
+	}
 });
