f64e9fd37fa8f3107886399d5ab25e1396290d17
Karolina Drazewska <drazewska.karolina@gmail.com>
2017-07-17 10:12:23 +0200

    shorthands

--- main.js
+++ main.js
@@ -1,35 +1,11 @@
 new Vue({
     el: '#app',
     data: {
-        counter: 0,
-        secondCounter: 0
-    },
-    // dependent properties
-    computed : {
-    	output: function() {
-    		return this.counter > 5 ? 'Greater 5' : 'Smaller than 5'
-    	}
-    },
-    // execute code upon data changes
-    watch: {
-    	counter: function(value) {
-    		var vm = this;
-    		setTimeout(function() {
-    			vm.counter = 0;
-    		}, 2000);
-    	}
+        link: 'http://google.com'
     },
     methods: {
-    	// increase: function() {
-    	// 	this.counter++;
-    	// 	this.result = this.counter > 5 ? 'Greater 5' : 'Smaller 5'
-    	// },
-    	// decrease: function() {
-    	// 	this.counter--;
-    	// 	this.result = this.counter > 5 ? 'Greater 5' : 'Smaller 5'
-    	// }
-    	result: function() {
-    		return this.counter > 5 ? 'Greater 5' : 'Smaller than 5'
+    	changeLink: function() {
+    		this.link = 'http://apple.com'
     	}
     }
 });
