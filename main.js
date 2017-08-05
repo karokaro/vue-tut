d8ef321cc6ee9b22c236ce9a8e6e074af803e1f6
karokaro <drazewska.karolina@gmail.com>
2017-08-05 15:08:02 +0200

    the vue instance lifecycle

--- main.js
+++ main.js
@@ -1,64 +1,35 @@
-var data = {
-	title: 'The VueJS Instance',
-    showParagraph: false
-}
-
-Vue.component('hello', {
-	template: '<h1>Hellllooo!</h1>'
-});
-
-var vm1 = new Vue({
-  data: data,
-  methods: {
-    show: function() {
-      this.showParagraph = true;
-      this.updateTitle('The VueJS Instance (Updated)');
-      //console.log(this.$refs);
-      this.$refs.myButton.innerText = 'New text for button';
-    },
-    updateTitle: function(title) {
-      this.title = title;
-    }
-  },
-  computed: {
-    lowercaseTitle: function() {
-      return this.title.toLowerCase();
-    }
-  },
-  watch: {
-    title: function(value) {
-      alert('Title changed, new value: ' + value);
-    }
-  }
-});
-
-vm1.$mount('#app');
-
-setTimeout(function() {
-	vm1.title = 'Changed by timer yeah!';
-	vm1.show();
-}, 4000);
-
-console.log(vm1.$data === data);
-vm1.$refs.heading.innerText = 'Something else';
-
-var vm2 = new Vue({
-  el: '#app2',
+new Vue({
+  el: '#app',
   data: {
-    title: 'The second VueJS Instance',
-    showParagraph: false
+  	title: 'The Vue Instance'
   },
-  methods : {
-  	changeSth: function() {
-  		vm1.title = 'Changed!'
+  beforeCreate: function() {
+  	console.log('beforeCreate()');
+  },
+  created: function() {
+  	console.log('created()');
+  },
+  beforeMount: function() {
+  	console.log('beforeMount()');
+  },
+  mounted: function() {
+  	console.log('mounted()');
+  },
+  beforeUpdate: function() {
+  	console.log('beforeUpdate()');
+  },
+  updated: function() {
+  	console.log('updated()');
+  },
+  beforeDestroy: function() {
+  	console.log('beforeDestroy()');
+  },
+  destroyed: function() {
+  	console.log('destroyed()');
+  },
+  methods: {
+  	destroy: function() {
+  		this.$destroy();
   	}
   }
 });
-
-var vm3 = new Vue({
-	template: '<h1>Heeelllooooo!</h1>'
-});
-
-//vm3.$mount('app3');
-vm3.$mount();
-document.getElementById('app3').appendChild(vm3.$el);
