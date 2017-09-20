61e094efa06e71e9bd4d548f71607d8181d89872
karokaro <drazewska.karolina@gmail.com>
2017-09-20 13:32:52 +0200

    components step 1

--- main.js
+++ main.js
@@ -1,35 +1,12 @@
-new Vue({
-  el: '#app',
-  data: {
-  	title: 'The Vue Instance'
-  },
-  beforeCreate: function() {
-  	console.log('beforeCreate()');
-  },
-  created: function() {
-  	console.log('created()');
-  },
-  beforeMount: function() {
-  	console.log('beforeMount()');
-  },
-  mounted: function() {
-  	console.log('mounted()');
-  },
-  beforeUpdate: function() {
-  	console.log('beforeUpdate()');
-  },
-  updated: function() {
-  	console.log('updated()');
-  },
-  beforeDestroy: function() {
-  	console.log('beforeDestroy()');
-  },
-  destroyed: function() {
-  	console.log('destroyed()');
-  },
-  methods: {
-  	destroy: function() {
-  		this.$destroy();
+Vue.component('my-comp', {
+	data: function() {
+  	return {
+  		status: 'Critical'
   	}
-  }
+  },
+  template: '<p>Server Status: {{status}}</p>'
+});
+
+new Vue({
+  el: '#app'
 });
